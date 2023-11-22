c1cf9e95437dd05f7cdb463f5d3e8a3183fcb7e1
Sorrypark <sorrypark1127@gmail.com>
2023-11-22 15:58:04 +0900

    Repo and Service, etc . .

--- src/main/java/com/PlannerService/Planner/Entity/DiscussionsEntity.java
+++ src/main/java/com/PlannerService/Planner/Entity/DiscussionsEntity.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 import static jakarta.persistence.FetchType.*;
 
