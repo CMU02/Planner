e7ac2e048b649edbb8092fc6a1e22b2d7a1490d1
CMU02 <hyeonjun1968@naver.com>
2023-11-25 22:15:10 +0900

    JWT Token 이용한 회원가입 및 로그인 API 구현 완료

--- src/main/java/com/PlannerService/Planner/Controller/UserController.java
+++ src/main/java/com/PlannerService/Planner/Controller/UserController.java
@@ -1,26 +1,18 @@
 package com.PlannerService.Planner.Controller;
 
-import com.PlannerService.Planner.DTO.UserDTO;
-import com.PlannerService.Planner.Service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController
 {
-    private final UserService userService;
-
-    @GetMapping("/User/new")
-    public String createdAccountForm() {
-        return "createdAccount";
-    }
-
-    @PostMapping("/User/new")
-    public String createdAccountUser(UserDTO userDTO) {
-        String userId = userService.join(userDTO);
-        return "createdAccount";
+    @GetMapping
+    public ResponseEntity<String> testing() {
+        return ResponseEntity.ok("Hi User");
     }
 }
