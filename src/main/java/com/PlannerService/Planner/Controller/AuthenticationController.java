f3e59b271b67ad53cc9fcc5c26a5113c328ee8ab
CMU02 <hyeonjun1968@naver.com>
2023-11-28 23:02:10 +0900

    대거 수정 완료

--- src/main/java/com/PlannerService/Planner/Controller/AuthenticationController.java
+++ src/main/java/com/PlannerService/Planner/Controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import com.PlannerService.Planner.DTO.SignUpRequest;
 import com.PlannerService.Planner.Entity.User;
 import com.PlannerService.Planner.Service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@Slf4j
 @RequiredArgsConstructor
 public class AuthenticationController
 {
--- src/main/java/com/PlannerService/Planner/Controller/UserController.java
+++ src/main/java/com/PlannerService/Planner/Controller/UserController.java
@@ -1,18 +1,27 @@
 package com.PlannerService.Planner.Controller;
 
+import com.PlannerService.Planner.DTO.MakePlanRequest;
+import com.PlannerService.Planner.Entity.Plan;
+import com.PlannerService.Planner.Service.PlanService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController
 {
-    @GetMapping
-    public ResponseEntity<String> testing() {
-        return ResponseEntity.ok("Hi User");
+    private final PlanService planService;
+    // 일정 추가 메서드
+    @PostMapping("/plan")
+    public ResponseEntity<Plan> addPlan(@RequestBody MakePlanRequest request)
+    {
+        Plan makePlan = planService.save(request);
+
+        // 요청한 자원을 성공적으로 생성 되었으며 저장된 일정의 정보를 응답 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(makePlan);
     }
 }
